0dd1650b68dbf2f4d07067bdebf0fb9d795d9945
andreas <andreas.bernerus@exertusit.se>
2016-06-12 16:42:49 +0200

    Updated script

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ echo "Killing old process"
 ps -ef | grep scrum | grep -v color | awk '{print $2}'
 kill `ps -ef | grep scrum | grep -v color | awk '{print $2}'`
 echo "Starting spring boot app!"
-java -Xmx64m -jar target/wedding-1.0.0.jar &
+java -Xmx64m -jar target/scrumpoker-0.0.1.jar &
